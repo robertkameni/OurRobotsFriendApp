6076542c78419831d8721000f703540309811f40
robertkameni <robertkameni83@gmail.com>
2020-04-10 01:38:46 +0200

    fixing security issues updated libraries

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,7 +6,7 @@ const searchBox = ({ searchfield, searchChange }) => {
 			<input
 				className='pa3 ba b--green bg-lightest-blue'
 				type='search'
-				placeholder='search robots'
+				placeholder='search a robot'
 				onChange={searchChange}
 			/>
 		</div>
