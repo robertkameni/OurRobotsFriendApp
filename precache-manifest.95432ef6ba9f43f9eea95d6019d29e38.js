20442b5d7b92b8653dfbf789b297999aa3805b63
Robert Kameni <robertkameni83@gmail.com>
2020-06-06 00:10:56 +0200

    Updates

---
+++ precache-manifest.95432ef6ba9f43f9eea95d6019d29e38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9b81716e9d35313e92bb475f80100e",
+    "url": "/OurRobotsFriendApp/index.html"
+  },
+  {
+    "revision": "2874531253396f27db50",
+    "url": "/OurRobotsFriendApp/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "88957b4b5401c6224d4a",
+    "url": "/OurRobotsFriendApp/static/css/main.bb38ee3a.chunk.css"
+  },
+  {
+    "revision": "2874531253396f27db50",
+    "url": "/OurRobotsFriendApp/static/js/2.7c474c73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/OurRobotsFriendApp/static/js/2.7c474c73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88957b4b5401c6224d4a",
+    "url": "/OurRobotsFriendApp/static/js/main.3356ee3a.chunk.js"
+  },
+  {
+    "revision": "2912e501eed341cd319d",
+    "url": "/OurRobotsFriendApp/static/js/runtime-main.c6e4da84.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/OurRobotsFriendApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/OurRobotsFriendApp/precache-manifest.95432ef6ba9f43f9eea95d6019d29e38.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/OurRobotsFriendApp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
